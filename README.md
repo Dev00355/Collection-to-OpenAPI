4d48698cafd26722afb109d33ff24dd8241c1d97
Dev <dev@Devs-MacBook-Pro.local>
2021-05-29 19:32:16 +0530

    Added OpanAPI spec to Postman collection

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # collection-to-openapi
-"collection-to-openapi" converts the postman collection to OpenAPI specification version 3.0.0.
+"collection-to-openapi" converts the postman collection to OpenAPI specification version 3.0.0 and also converts the openAPI to postman collection.
 
-This extension is based on the open source NodeJs library "postman-to-openapi" https://www.npmjs.com/package/postman-to-openapi
+This extension is based on the open source NodeJs libraries "postman-to-openapi" https://www.npmjs.com/package/postman-to-openapi and "openapi-to-postmanv2" https://www.npmjs.com/package/openapi-to-postmanv2 
 
 ## Tech
 Collection to OpenAPI is built with Javascript. No javascript frameworks used.
@@ -10,13 +10,15 @@ Collection to OpenAPI is built with Javascript. No javascript frameworks used.
 
 Open the Postman collection json file and press Ctrl+Shift+P (Windows User), Cmd+Shift+P(Mac User) and type command "Convert to OpenAPI". Extension automatically creates the OpenAPI specification file with "yml" extension in the same directory.
 
+To convert the OpenAPI specification to postman collection type command "Convert to Postman Collection" in command pallet.
+
 ## Known Issues
 
-Only works with Postman collection json files. 
+Only works with Postman collection. 
 
 ## Release Notes
 
-### 0.1.0
+### 1.0.0
 
 Initial release of collection-to-openapi, includes postman collection json file conversion to OpenAPI specification
 
--- extension.js
+++ extension.js
@@ -1,8 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require("fs");
 const postmanToOpenApi = require('postman-to-openapi');
-
+const openApiToPostman = require('openapi-to-postmanv2');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -40,8 +41,54 @@ function activate(context) {
 		// Display a message box to the user
 		// vscode.window.showInformationMessage('Hello World from Collection to OpenAPI!');
 	});
-
+	let disposable1 = vscode.commands.registerCommand('collection-to-openapi.convert-to-collection', function () {
+		// The code you place here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
+		let collectionFile;
+		if(editor){
+			const document = editor.document;
+			let sourceFile = document.fileName.toString();
+			if( (sourceFile.includes('yml')) || (sourceFile.includes('yaml'))){
+				if( sourceFile.includes('yml') ) {
+					collectionFile = sourceFile.replace(".yml", ".json");
+				} else {
+					collectionFile = sourceFile.replace(".yaml", ".json");
+				}
+			} else {
+				collectionFile = sourceFile.split('.');
+				collectionFile = collectionFile[0] + "_collection." + collectionFile[1];
+			}
+			console.log(`collection filename ${collectionFile}`);
+			openapidata = fs.readFileSync(sourceFile, { encoding: "UTF-8" });
+			openApiToPostman.convert(
+				{ type: "string", data: openapidata },
+				{},
+				(err, conversionResult) => {
+					if (!conversionResult.result) {
+						console.log('Could not convert', conversionResult.reason);
+					}else {
+						fs.writeFile(
+							collectionFile,
+							JSON.stringify(conversionResult.output[0].data),
+							(err)=>{
+								if(err) console.log(err);
+								else {
+									vscode.window.showInformationMessage('File Converted!');
+								}
+							}
+						);
+					}
+				}
+			);
+			
+		}else{
+			vscode.window.showErrorMessage('Please select valid OpanAPI file');
+		}
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Hello World from Collection to OpenAPI!');
+	});
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable1);
 }
 
 // this method is called when your extension is deactivated
